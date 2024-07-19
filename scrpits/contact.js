e3ad2b31d518118e0bfd44aa0551cce1cf7deca4
AlejandroClaure <alejandroclaure01@gmail.com>
2024-07-19 19:22:08 -0300

    modified script in contact(emailjs)

--- scrpits/contact.js
+++ scrpits/contact.js
@@ -1,14 +1,16 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
+document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
-    
-    const statusMessage = document.getElementById('status-message');
-    statusMessage.textContent = "Enviando...";
 
-    emailjs.sendForm('service_1alec1', 'template_d7zgrey', this)
-        .then(function() {
-            statusMessage.textContent = "Mensaje enviado correctamente.";
-            document.getElementById('contact-form').reset(); // Limpia los campos del formulario
-        }, function(error) {
-            statusMessage.textContent = "Error al enviar el mensaje: " + JSON.stringify(error);
+    const serviceID = 'service_1alec1'; // Sustituye "YOUR_SERVICE_ID" con tu Service ID de EmailJS
+    const templateID = 'template_d7zgrey'; // Sustituye "YOUR_TEMPLATE_ID" con tu Template ID de EmailJS
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Mensaje enviado exitosamente!');
+            document.getElementById('contact-form').reset();
+        }, function (error) {
+            console.error('FAILED...', error);
+            alert('Error al enviar el mensaje, por favor intenta nuevamente.');
         });
 });
